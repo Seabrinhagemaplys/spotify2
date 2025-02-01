b12c01673ebe518f42ba450500570ef03d5173fc
juanJunq <juan.marquesjunq@gmail.com>
2025-02-01 14:40:37 -0300

    feat: eslint.json configurado

--- src/domains/music/services/musicServices.ts
+++ src/domains/music/services/musicServices.ts
@@ -3,65 +3,65 @@ import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
 
 class MusicService {
-  // Método para criar uma nova música
-  async createMusic(
-    nome: string,
-    genero: string,
-    ID_Artista: number,
-    album?: string
-  ) {
-    return prisma.musica.create({
-      data: { nome, genero, album, ID_Artista },
-    });
-  }
-  // Método para buscar uma música pelo ID
-  async getMusicById(id: number) {
-    return prisma.musica.findUnique({
-      where: { ID_Musica: id },
-      include: { artista: true },
-    });
-  }
-  // Método para buscar todas as músicas cadastradas
-  async getAllMusic() {
-    return prisma.musica.findMany();
-  }
+	// Método para criar uma nova música
+	async createMusic(
+		nome: string,
+		genero: string,
+		ID_Artista: number,
+		album?: string
+	) {
+		return prisma.musica.create({
+			data: { nome, genero, album, ID_Artista },
+		});
+	}
+	// Método para buscar uma música pelo ID
+	async getMusicById(id: number) {
+		return prisma.musica.findUnique({
+			where: { ID_Musica: id },
+			include: { artista: true },
+		});
+	}
+	// Método para buscar todas as músicas cadastradas
+	async getAllMusic() {
+		return prisma.musica.findMany();
+	}
 
-  async updateMusica(id: number, body: any) {
-    try {
-      const musicaFound = await prisma.musica.findUnique({
-        where: { ID_Musica: id },
-      });
+	async updateMusica(id: number, body: any) {
+		try {
+			const musicaFound = await prisma.musica.findUnique({
+				where: { ID_Musica: id },
+			});
 
-      if (!musicaFound) {
-        throw new Error("Música não encontrada.");
-      }
+			if (!musicaFound) {
+				throw new Error("Música não encontrada.");
+			}
 
-      const updatedMusica = await prisma.musica.update({
-        data: {
-          nome: body.nome,
-          genero: body.genero,
-          album: body.album,
-          ID_Artista: body.ID_Artista,
-        },
-        where: { ID_Musica: id },
-      });
+			const updatedMusica = await prisma.musica.update({
+				data: {
+					nome: body.nome,
+					genero: body.genero,
+					album: body.album,
+					ID_Artista: body.ID_Artista,
+				},
+				where: { ID_Musica: id },
+			});
 
-      return updatedMusica;
-    } catch (error) {
-      console.error("Erro ao atualizar música: ", error);
-      throw new Error("Não foi possível atualizar a música.");
-    }
-  }
+			return updatedMusica;
+		} catch (error) {
+			console.error("Erro ao atualizar música: ", error);
+			throw new Error("Não foi possível atualizar a música.");
+		}
+	}
 
-  async deleteMusica(id: number) {
-    try {
-      await prisma.musica.delete({
-        where: { ID_Musica: id },
-      });
-      console.log("Música deletada com sucesso.");
-    } catch (error) {
-      console.error("Erro ao deletar música:", error);
-      throw new Error("Não foi possível deletar a música.");
-    }
-  }
+	async deleteMusica(id: number) {
+		try {
+			await prisma.musica.delete({
+				where: { ID_Musica: id },
+			});
+			console.log("Música deletada com sucesso.");
+		} catch (error) {
+			console.error("Erro ao deletar música:", error);
+			throw new Error("Não foi possível deletar a música.");
+		}
+	}
 }
--- src/domains/userMusic/services/UserMusicServices.ts
+++ src/domains/userMusic/services/UserMusicServices.ts
@@ -3,27 +3,27 @@ import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
 
 class UserMusicService {
-    // Método para adicionar uma música à lista do usuário
-  async addMusicToUser(ID_Usuario: number, ID_Musica: number) {
-    return prisma.usuarios_Musicas.create({
-      data: { ID_Usuario, ID_Musica },
-    });
-  }
-//Método para remover uma música da lista do usuário
-  async removeMusicFromUser(ID_Usuario: number, ID_Musica: number) {
-    return prisma.usuarios_Musicas.delete({
-      where: { 
-        ID_Usuario_ID_Musica: { ID_Usuario, ID_Musica } // Verifique se essa é realmente a chave no Prisma
-      },
-    });
-  }
-//Método para buscar todas as músicas de um usuário definido
-  async getUserMusic(ID_Usuario: number) {
-    return prisma.usuarios_Musicas.findMany({
-      where: { ID_Usuario },
-      include: { musica: true },
-    });
-  }
+	// Método para adicionar uma música à lista do usuário
+	async addMusicToUser(ID_Usuario: number, ID_Musica: number) {
+		return prisma.usuarios_Musicas.create({
+			data: { ID_Usuario, ID_Musica },
+		});
+	}
+	//Método para remover uma música da lista do usuário
+	async removeMusicFromUser(ID_Usuario: number, ID_Musica: number) {
+		return prisma.usuarios_Musicas.delete({
+			where: { 
+				ID_Usuario_ID_Musica: { ID_Usuario, ID_Musica } // Verifique se essa é realmente a chave no Prisma
+			},
+		});
+	}
+	//Método para buscar todas as músicas de um usuário definido
+	async getUserMusic(ID_Usuario: number) {
+		return prisma.usuarios_Musicas.findMany({
+			where: { ID_Usuario },
+			include: { musica: true },
+		});
+	}
 }
 
 const userMusicService = new UserMusicService();
