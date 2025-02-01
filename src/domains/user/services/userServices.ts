4780a4c08061c59d3afc4f0b8e1a4610f3476bc0
Thales Faria <thales05faria@gmail.com>
2025-02-01 13:39:07 -0300

    feat: creation of update and delete methods of userService

--- src/domains/user/services/userServices.ts
+++ src/domains/user/services/userServices.ts
@@ -1,6 +1,45 @@
 import prisma from "../../../../database/prismaClient";
+import { User } from "@prisma/client";
 
-class userService{
+class userService {
+  async updateUser(email: string, body: User) {
+    try {
+      // Verifica se o usuário existe
+      const userFound = await prisma.user.findUnique({
+        where: { email },
+      });
 
-    
+      if (!userFound) {
+        throw new Error("Usuário não encontrado.");
+      }
+
+      const updatedUser = await prisma.user.update({
+        data: {
+          username: body.username,
+          email: body.email,
+          senha: body.senha,
+          foto_user: body.foto_user,
+        },
+        where: {
+          email: email,
+        },
+      });
+
+      return updatedUser;
+    } catch (error) {
+      console.error("Erro ao atualizar os dados: ", error);
+      throw new Error("Não foi possível atualizar os dados.");
+    }
+  }
+  async deleteUser(email: string) {
+    try {
+      await prisma.user.delete({
+        where: { email },
+      });
+      console.log("Usuário deletado com sucesso.");
+    } catch (error) {
+      console.error("Erro ao deletar usuário:", error);
+      throw new Error("Não foi possível deletar o usuário.");
+    }
+  }
 }
