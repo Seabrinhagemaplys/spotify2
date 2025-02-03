3eed383bca75519edaba66f7d72ae8375e4bf9be
Thales Faria <thales05faria@gmail.com>
2025-02-03 11:33:11 -0300

    fix: Correction of instance names in updateUser

--- src/domains/user/services/userServices.ts
+++ src/domains/user/services/userServices.ts
@@ -1,71 +1,73 @@
 import { PrismaClient } from "@prisma/client";
+import { Usuario } from "@prisma/client";
 const prisma = new PrismaClient();
 
 // Usuário Service
 class UserService {
-	// Método para criar um novo usuário
-	async createUser(
-		nome: string,
-		email: string,
-		senha: string,
-		admin: boolean = false,
-		foto?: string
-	) {
-		return prisma.usuario.create({
-			data: { nome, email, senha, admin, foto },
-		});
-	}
-	//Método para buscar um usuário pelo ID com suas músicas associadas
-	async getUserById(id: number) {
-		return prisma.usuario.findUnique({
-			where: { ID_Usuario: id },
-			include: { musicas: true }, //musicas conectados ao usuario
-		});
-	}
-	//Método para buscar todos os usuários cadastrados
-	async getAllUsers() {
-		return prisma.usuario.findMany();
-	}
+  // Método para criar um novo usuário
+  async createUser(
+    nome: string,
+    email: string,
+    senha: string,
+    admin: boolean = false,
+    foto?: string
+  ) {
+    return prisma.usuario.create({
+      data: { nome, email, senha, admin, foto },
+    });
+  }
+  //Método para buscar um usuário pelo ID com suas músicas associadas
+  async getUserById(id: number) {
+    return prisma.usuario.findUnique({
+      where: { ID_Usuario: id },
+      include: { musicas: true }, //musicas conectados ao usuario
+    });
+  }
+  //Método para buscar todos os usuários cadastrados
+  async getAllUsers() {
+    return prisma.usuario.findMany();
+  }
 
-	async updateUser(email: string, body: User) {
-		try {
-			// Verifica se o usuário existe
-			const userFound = await prisma.user.findUnique({
-				where: { email },
-			});
+  async updateUser(email: string, body: Partial<Usuario>) {
+    try {
+      // Verifica se o usuário existe
+      const userFound = await prisma.usuario.findUnique({
+        where: { email },
+      });
 
-			if (!userFound) {
-				throw new Error("Usuário não encontrado.");
-			}
+      if (!userFound) {
+        throw new Error("Usuário não encontrado.");
+      }
 
-			const updatedUser = await prisma.user.update({
-				data: {
-					username: body.username,
-					email: body.email,
-					senha: body.senha,
-					foto_user: body.foto_user,
-				},
-				where: {
-					email: email,
-				},
-			});
+      const updatedUser = await prisma.usuario.update({
+        data: {
+          nome: body.nome,
+          email: body.email,
+          senha: body.senha,
+          foto: body.foto,
+          admin: body.admin,
+        },
+        where: {
+          email: email,
+        },
+      });
 
-			return updatedUser;
-		} catch (error) {
-			console.error("Erro ao atualizar os dados: ", error);
-			throw new Error("Não foi possível atualizar os dados.");
-		}
-	}
-	async deleteUser(email: string) {
-		try {
-			await prisma.user.delete({
-				where: { email },
-			});
-			console.log("Usuário deletado com sucesso.");
-		} catch (error) {
-			console.error("Erro ao deletar usuário:", error);
-			throw new Error("Não foi possível deletar o usuário.");
-		}
-	}
+      return updatedUser;
+    } catch (error) {
+      console.error("Erro ao atualizar os dados: ", error);
+      throw new Error("Não foi possível atualizar os dados.");
+    }
+  }
+  async deleteUser(email: string) {
+    try {
+      await prisma.usuario.delete({
+        where: { email },
+      });
+      console.log("Usuário deletado com sucesso.");
+    } catch (error) {
+      console.error("Erro ao deletar usuário:", error);
+      throw new Error("Não foi possível deletar o usuário.");
+    }
+  }
 }
 export default new UserService();
