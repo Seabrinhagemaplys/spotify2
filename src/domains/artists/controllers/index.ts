aaa6cfdb16a7f1d7183d950fd84ee42f46987100
Thales Faria <thales05faria@gmail.com>
2025-02-07 13:47:42 -0300

    feat: middlewares and request status added to artist routes

--- src/domains/artists/controllers/index.ts
+++ src/domains/artists/controllers/index.ts
@@ -1,5 +1,8 @@
 import { Router, Request, Response, NextFunction } from "express";
 import ArtistService  from "../services/artistsServices";
+import { checkRole, verifyJWT } from "../../../middlewares/auth";
+import statusCodes from "../../../../utils/constants/statusCodes";
+
 
 const router = Router();
 
@@ -7,7 +10,7 @@ const router = Router();
 router.get("/", verifyJWT, async (req: Request, res: Response, next: NextFunction) => {
 	try{
 		const artists = await ArtistService.getAllArtists();
-		res.json(artists);
+		res.status(statusCodes.SUCCESS).json(artists);
 	} catch(error) {
 		next(error);
 	}
@@ -17,44 +20,44 @@ router.get("/", verifyJWT, async (req: Request, res: Response, next: NextFunctio
 router.get("/:id", verifyJWT, async (req: Request, res: Response, next: NextFunction) => {
 	try{
 		const artist = await ArtistService.getArtistById(Number(req.params.id));
-		res.json(artist);
+		res.status(statusCodes.SUCCESS).json(artist);
 	} catch(error) {
 		next(error);
 	}
 });
 
 //Middlewares: verifyJWT e checkRole (verifica se o usuário é ADMIN ou não)
-router.post("/", verifyJWT, checkRole(), async (req: Request, res: Response, next: NextFunction) => {
+router.post("/", verifyJWT, checkRole, async (req: Request, res: Response, next: NextFunction) => {
 	try{
 		const { nome, foto, numero_streams } = req.body;
 
 		const newArtist = await ArtistService.createArtist(nome, foto, numero_streams);
-		res.json(newArtist);
+		res.status(statusCodes.CREATED).json(newArtist);
 	} catch (error){
 		next(error);
 	}
 });
 
 //Middlewares: verifyJWT e checkRole
-router.put("/:id", verifyJWT, checkRole(), async (req: Request, res: Response, next: NextFunction) =>{
+router.put("/:id", verifyJWT, checkRole, async (req: Request, res: Response, next: NextFunction) =>{
 	try{
 		const { id } = req.params;
 		const body  = req.body;
 		const idArtist = Number(id);
 		const updatedArtista = await ArtistService.updateArtista(idArtist, body);
-		res.json(updatedArtista);
+		res.status(statusCodes.SUCCESS).json(updatedArtista);
 	} catch(error) {
 		next(error);
 	}
 });
 
 //Middlewares: verifyJWT e checkRole
-router.delete("/:id", verifyJWT, checkRole(), async (req: Request, res: Response, next: NextFunction) => {
+router.delete("/:id", verifyJWT, checkRole, async (req: Request, res: Response, next: NextFunction) => {
 	try{
 		const { id } = req.params;
 		const idArtist = Number(id);
 		await ArtistService.deleteArtista(idArtist);
-		res.send("Artista deletado");
+		res.status(statusCodes.NO_CONTENT).send("Artista deletado");
 	} catch(error) {
 		next(error);
 	}
